c22e04d46ce850a478b8c2f11bee5d8c675d3ffa
Luiza-l <marialuizanunes331@gmail.com>
2026-06-04 22:22:49 -0300

    feat: add CalculadoraSimples

--- Operadores/src/exercicios/CalculadoraSimples.java
+++ Operadores/src/exercicios/CalculadoraSimples.java
@@ -4,7 +4,18 @@ public class CalculadoraSimples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 2;
+		int b = 30;
+		
+		int soma = a + b;
+		int subtracao = a - b;
+		int mult = a*b;
+		int resto = a%b;
+		
+		System.out.println("Soma: "+ soma);
+		System.out.println("Subtração: "+ subtracao);
+		System.out.println("Multiplicação: "+ mult);
+		System.out.println("Resto: "+ resto);
 	}
 
 }
